95e1aaa4027dc28fe4ac0b740e816bae3be5cc64
Expired Milk <domasdar2@gmail.com>
2024-04-10 14:20:38 -0500

    got first api call to work

--- controllers/api/workoutRoutes.js
+++ controllers/api/workoutRoutes.js
@@ -1,7 +1,20 @@
 const router = require('express').Router();
 const { Workout } = require('../../models');
 
+router.get('/:exerciseType', async (req, res) => {
+    try {
+        const response = await fetch(`https://api.api-ninjas.com/v1/exercises?muscle=${req.params.exerciseType}`, {
+            method: "GET",
+            headers: {
+                'X-Api-Key': process.env.API_KEY
+            }
+        });
 
-
+        const result = await response.json();
+        console.log(result);
+    } catch (error) {
+        console.log('Error:', error);
+    }
+})
 
 module.exports = router
