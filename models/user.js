520109a197c76c7fb97fc3bbbe6fef9f1815a8e4
Nate Johnson <njohnson2897@gmail.com>
2024-04-11 08:50:19 -0500

    builds out models for User and Workout and establishes model relationships in index.js

--- models/user.js
+++ models/user.js
@@ -4,9 +4,40 @@ const sequelize = require('../config/connection');
 
 class User extends Model {}
 
-
-
-
-
+User.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                isEmail: true,
+            },
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [8],
+            },
+        },
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'activ_user',
+    }
+);
 
 module.exports = User;
